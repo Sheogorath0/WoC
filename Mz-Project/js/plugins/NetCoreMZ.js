b31e24624c0e032cab3b159cbc12174b66e71fb1
Sheogorath0 <baewocom@gmail.com>
2026-05-31 01:39:41 +0900

    backup1

--- Mz-Project/js/plugins/NetCoreMZ.js
+++ Mz-Project/js/plugins/NetCoreMZ.js
@@ -1,6 +1,6 @@
 /*:
  * @target MZ
- * @plugindesc MZ 20인 MMO - [15단계] 캐릭터 외형(이미지/인덱스) 완벽 실시간 동기화 빌드
+ * @plugindesc MZ 20인 MMO - [13단계] 새 맵 진입 시 정지 유저 스폰 누락 버그 완벽 해결판
  * @author 코딩 파트너
  */
 
@@ -19,35 +19,25 @@
     function Game_NetPlayer() { this.initialize(...arguments); }
     Game_NetPlayer.prototype = Object.create(Game_Character.prototype);
     Game_NetPlayer.prototype.constructor = Game_NetPlayer;
-
     Game_NetPlayer.prototype.initialize = function (userId, data) {
         Game_Character.prototype.initialize.call(this);
         this._userId = userId;
         this.setPosition(data.x, data.y);
-
-        // [비주얼 동기화 핵심] 상대방 캐릭터 생성 시 서버가 준 이미지 강제 적용
         this.setImage(data.characterName, data.characterIndex);
-
         this.setDirection(data.d || 2);
         this.setMoveSpeed(data.moveSpeed || 4);
         this.setThrough(true);
         this.setStepAnime(true);
         this._sprite = null;
     };
-
     Game_NetPlayer.prototype.updateData = function (data) {
         this.setDirection(data.d);
         if (data.moveSpeed !== undefined) this.setMoveSpeed(data.moveSpeed);
-
         const diffX = Math.abs(this._x - data.x);
         const diffY = Math.abs(this._y - data.y);
         if (diffX > 1 || diffY > 1) this.setPosition(data.x, data.y);
         else { this._x = data.x; this._y = data.y; }
-
-        // [비주얼 동기화 핵심] 이동 패킷을 받을 때마다 현재 외형이 바뀌었는지 체크하고 강제 리프레시
-        if (this._characterName !== data.characterName || this._characterIndex !== data.characterIndex) {
-            this.setImage(data.characterName, data.characterIndex);
-        }
+        this.setImage(data.characterName, data.characterIndex);
     };
 
     function Sprite_NetNameTag() { this.initialize(...arguments); }
@@ -120,8 +110,6 @@
                 console.log(`[Network] ${myNetId} 로그인 성공!`);
 
                 DataManager.setupNewGame();
-
-                // [중요] 로그인 성공 직후 내 로컬 캐릭터 이미지를 서버가 지정한 이미지로 강제 오버라이딩
                 $gamePlayer.setImage(packet.characterName, packet.characterIndex);
                 $gamePlayer.reserveTransfer(packet.mapId || 1, packet.x, packet.y, 2, 0);
 
@@ -140,15 +128,19 @@
                 break;
 
             case 'REFRESH_MAP_PLAYERS':
+                // [신규] 맵 이동 직후 서버가 보내준 "새 맵의 고인물 정지 유저 목록"을 일괄 생성
                 if (packet.existingPlayers) {
                     for (const id in packet.existingPlayers) {
                         if (id !== myNetId && !remotePlayers[id]) {
                             remotePlayers[id] = new Game_NetPlayer(id, packet.existingPlayers[id]);
                         }
                     }
+                    // 주입 후 화면 스프라이트 셋업 갱신
                     if (SceneManager._scene instanceof Scene_Map && SceneManager._scene._spriteset) {
                         const spriteset = SceneManager._scene._spriteset;
-                        for (const id in remotePlayers) { addNetSpriteToScene(spriteset, remotePlayers[id]); }
+                        for (const id in remotePlayers) {
+                            addNetSpriteToScene(spriteset, remotePlayers[id]);
+                        }
                     }
                 }
                 break;
@@ -175,12 +167,10 @@
     Game_Player.prototype.reserveTransfer = function (mapId, x, y, d, fadeType) {
         if (isLoggedIn && $gameMap && $gameMap.mapId() !== mapId) {
             if (socket && socket.readyState === WebSocket.OPEN) {
-                // 이사 가기 전에도 실제 화면에 그려진 내 렌더링 속성을 추출하여 전송
                 socket.send(JSON.stringify({
                     type: 'MOVE',
                     mapId: mapId, x: x, y: y, d: d,
-                    characterName: this._characterName,
-                    characterIndex: this._characterIndex,
+                    characterName: this.characterName(), characterIndex: this.characterIndex(),
                     moveSpeed: this.realMoveSpeed()
                 }));
             }
@@ -192,6 +182,8 @@
     Spriteset_Map.prototype.createCharacters = function () {
         clearAllRemotePlayers();
         _Spriteset_Map_createCharacters.call(this);
+
+        // [버그 수정 핵심] 새 맵의 캐릭터 레이어가 다 그려졌으므로, 서버에 명단 갱신 전용 요청을 쏩니다.
         if (isLoggedIn && $gameMap) {
             socket.send(JSON.stringify({
                 type: 'MAP_CHANGED',
@@ -214,7 +206,6 @@
         sprite.addChild(nameTag);
     }
 
-    // 다른 맵에서 내 방으로 이사 온 유저 이미지 동기화 보장
     function updateRemotePlayer(userId, data) {
         if (userId === myNetId) return;
         if (!remotePlayers[userId]) remotePlayers[userId] = new Game_NetPlayer(userId, data);
@@ -239,17 +230,12 @@
         if (isLoggedIn) sendMovementPacket();
     };
 
-    // [핵심 변경] $gamePlayer.characterName() 대신 실시간 언더바 변수인 _characterName을 직접 털어서 보냅니다.
     function sendMovementPacket() {
         if (socket && socket.readyState === WebSocket.OPEN && $gameMap) {
             socket.send(JSON.stringify({
                 type: 'MOVE',
-                mapId: $gameMap.mapId(),
-                x: $gamePlayer.x,
-                y: $gamePlayer.y,
-                d: $gamePlayer.direction(),
-                characterName: $gamePlayer._characterName,
-                characterIndex: $gamePlayer._characterIndex,
+                mapId: $gameMap.mapId(), x: $gamePlayer.x, y: $gamePlayer.y, d: $gamePlayer.direction(),
+                characterName: $gamePlayer.characterName(), characterIndex: $gamePlayer.characterIndex(),
                 moveSpeed: $gamePlayer.realMoveSpeed()
             }));
         }
--- mmo-server/userdata.js
+++ mmo-server/userdata.js
@@ -1,27 +1,24 @@
 const userDatabase = {
   "admin": {
     "password": "123",
-    "characterName": "Actor1",
-    "characterIndex": 0,
-    "x": 8,
-    "y": 4,
-    "mapId": 1
+    "actorId": 1,
+    "mapId": 1,
+    "x": 6,
+    "y": 5
   },
   "player1": {
     "password": "123",
-    "characterName": "Actor2",
-    "characterIndex": 1,
-    "x": 15,
-    "y": 6,
-    "mapId": 1
+    "actorId": 2,
+    "mapId": 1,
+    "x": 3,
+    "y": 7
   },
   "player2": {
     "password": "123",
-    "characterName": "Actor3",
-    "characterIndex": 2,
-    "x": 4,
-    "y": 5,
-    "mapId": 1
+    "actorId": 3,
+    "mapId": 1,
+    "x": 8,
+    "y": 5
   }
 };
 
